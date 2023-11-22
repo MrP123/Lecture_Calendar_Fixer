5c17edcd57ce787ad02f97d3abc0daf5bf623687
Matthias Panny <matthias.panny@mci.edu>
2023-11-22 14:56:35 +0100

    Added run script for Windows Aufgabenplanung

---
+++ run_script.bat
@@ -0,0 +1,2 @@
+@echo off
+"H:\02_Projekte\310_Python_Office_Automation\lecture_calendar_fixer\.venv\Scripts\python.exe" "H:\02_Projekte\310_Python_Office_Automation\lecture_calendar_fixer\lecture_calendar_fixer.py"
