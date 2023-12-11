2511ced89276ee26fb5ee4b843ef965aec9688f5
Matthias Panny <matthias.panny@mci.edu>
2023-12-11 15:48:30 +0100

    Added a first version where only events that have changed are updated. This still needs to be verified fully.

--- lecture_calendar_fixer.py
+++ lecture_calendar_fixer.py
@@ -41,6 +41,52 @@ def add_lecture_events_to_outlook(webcalendar, outlook):
         logging.info("\nAdding event:\n\t" + wrapper)
         wrapper.to_outlook_event(outlook)
 
+def try_deleting_outlook_appointment(appointment):
+    should_retry = 1
+    tries = 0
+    while should_retry > 0 and tries < 5:   
+        tries += 1
+        should_retry -= 1
+        try:
+            appointment.Delete()
+        except:
+            logging.warn("Could not delete appointment")
+            should_retry += 1
+
+def update_changed_events(webcalender, outlook):
+    outlook_calendar = outlook.GetNamespace("MAPI").GetDefaultFolder(9) #calender = 9
+    appointments = outlook_calendar.Items
+
+    found_appointments = [appointment for appointment in appointments if EventWrapper.get_default_organizer() in getattr(appointment, "Organizer", "Error")]
+    logging.info(F"Found {len(found_appointments)} outlook appointments")
+    outlook_appointment_dict = {appointment.Organizer: appointment for appointment in found_appointments}
+
+    all_events = [subcomp for subcomp in webcalendar.subcomponents if subcomp.name == "VEVENT"]
+    lecture_events = [event for event in all_events if not "Abgabetermin" in event['summary']]
+    logging.info(F"Found {len(lecture_events)} ical events")
+
+    for event in lecture_events:
+        ical_event_wrapped = EventWrapper.from_ical_event(event)
+
+        # get corresponding outlook appointment from dict
+        corresponding_outlook_appointment = outlook_appointment_dict.get(ical_event_wrapped.organizer)
+        if corresponding_outlook_appointment:
+
+            # if found wrap it for comparison
+            outlook_appointment_wrapped = EventWrapper.from_outlook_event(corresponding_outlook_appointment)
+            if ical_event_wrapped != outlook_appointment_wrapped:
+                # event has changed --> delete and add again
+                logging.info("\nTrying to delete appointment:\n\t" + outlook_appointment_wrapped)
+                try_deleting_outlook_appointment(corresponding_outlook_appointment)
+
+                logging.info("\nAdding event:\n\t" + ical_event_wrapped)
+                ical_event_wrapped.to_outlook_event(outlook)
+            else:
+                logging.info("\nAppointment is up to date:\n\t" + ical_event_wrapped)
+        else:
+            # if it is not available then add it
+            logging.info("\nAdding event:\n\t" + ical_event_wrapped)
+            ical_event_wrapped.to_outlook_event(outlook)
 
 if __name__ == "__main__":
     load_dotenv()
@@ -61,5 +107,7 @@ if __name__ == "__main__":
     webcalendar = icalendar.Calendar.from_ical(response.text)
     outlook = win32com.client.Dispatch("Outlook.Application")
 
-    delete_all_existing_lecture_events(outlook)
-    add_lecture_events_to_outlook(webcalendar, outlook)
+    #delete_all_existing_lecture_events(outlook)
+    #add_lecture_events_to_outlook(webcalendar, outlook)
+
+    update_changed_events(webcalendar, outlook)
